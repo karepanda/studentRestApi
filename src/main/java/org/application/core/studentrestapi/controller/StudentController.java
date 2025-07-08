bbea1e47e0b0ecee3e50f569feb2d25d0fdd947f
kare <karelysperezs@gmail.com>
2025-07-08 18:43:40 +0200

    add CRUD endpoints for Student entity in StudentController

--- src/main/java/org/application/core/studentrestapi/controller/StudentController.java
+++ src/main/java/org/application/core/studentrestapi/controller/StudentController.java
@@ -1,12 +1,40 @@
 package org.application.core.studentrestapi.controller;
 
+import org.application.core.studentrestapi.entities.Student;
 import org.application.core.studentrestapi.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
 
     @Autowired
     StudentRepository studentRepository;
+
+    @GetMapping("/student/")
+    public List<Student> getAllProducts() {
+        return studentRepository.findAll();
+    }
+
+    @GetMapping("/student/{id}")
+    public Student getProduct(@PathVariable("id") int id){
+        return studentRepository.findById(id).get();
+    }
+
+    @PostMapping("/student/")
+    public Student createProduct(@RequestBody Student product){
+        return studentRepository.save(product);
+    }
+
+    @PutMapping("/student/")
+    public Student updateProduct(@RequestBody Student product){
+        return studentRepository.save(product);
+    }
+
+    @DeleteMapping("/student/{id}")
+    public void deleteProduct(@PathVariable("id") int id){
+        studentRepository.deleteById(id);
+    }
 }
